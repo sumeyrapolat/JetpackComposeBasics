ca623f45795dd888068c4f4d9d9dd33325e9f08e
Sümeyra Polat <sumeyrapolaat@gmail.com>
2024-05-05 00:54:47 +0300

    Activity Gradle and Manifest updated.

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,9 +5,17 @@ import android.telecom.Call
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.jetpackcompose.ui.theme.Coil.CoilImage
+import com.example.jetpackcompose.ui.theme.Coil.CoilImagePreview
 import com.example.jetpackcompose.ui.theme.TextField.MyTextField
 import com.example.jetpackcompose.ui.theme.ExpandableCard.ExpandableCard
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.example.jetpackcompose.ui.theme.Password.PasswordComp
 import com.example.jetpackcompose.ui.theme.SignUpWithGoogle.SignUpWithGoogle
 import com.example.jetpackcompose.ui.theme.SubScript.CallSubScriptText
 import com.example.jetpackcompose.ui.theme.SuperScript.CallSuperScriptText
@@ -19,14 +27,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-            //ExpandableCard()
-            //TextSelection()
-            //CallSuperScriptText()
-            // CallSubScriptText()
-            // MyTextField()
-                SignUpWithGoogle("Sign Up With Google", "Account Loading...", onClicked = {
-                    Log.i("google button", "clicked")
-                })
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    //ExpandableCard()
+                    //TextSelection()
+                    //CallSuperScriptText()
+                    // CallSubScriptText()
+                    // MyTextField()
+                    /* SignUpWithGoogle("Sign Up With Google", "Account Loading...", onClicked = {
+                        Log.i("google button", "clicked")
+                    }*/
+                        //CoilImage()
+                    PasswordComp()
+                }
+
+                //CoilImage()
+
+
             }
         }
     }
